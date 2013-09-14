0a45b468164e18a15d1c54b2f3c047953504fc61
skosmalla <skosmallaweb.de>
2013-09-14 18:16:33 +0200

    refactoring

---
+++ flyway-demo/demo-web-application/src/main/java/com/github/skosmalla/flyway/demo/webapp/PersonListView.java
@@ -0,0 +1,27 @@
+package com.github.skosmalla.flyway.demo.webapp;
+
+import java.util.List;
+
+import com.github.skosmalla.flyway.demo.webapp.domain.Person;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+
+final class PersonListView extends ListView<Person> {
+
+    /**  */
+    private static final long serialVersionUID = 9172837350405031400L;
+
+
+    PersonListView(String id, List<? extends Person> list) {
+        super(id, list);
+    }
+
+
+    @Override
+    protected void populateItem(ListItem<Person> item) {
+      Person person = item.getModelObject();
+      item.add(new Label("firstname", person.getFirstName()));
+      item.add(new Label("lastname", person.getLastName()));
+    }
+}
