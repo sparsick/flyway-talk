f5646add10a6a4a6ab8c4a3b6742de915e04ff41
skosmalla <skosmallaweb.de>
2013-09-15 20:09:53 +0200

    fix test

--- flyway-demo/demo-web-application/src/test/java/com/github/skosmalla/flyway/demo/webapp/TestHomePage.java
+++ flyway-demo/demo-web-application/src/test/java/com/github/skosmalla/flyway/demo/webapp/TestHomePage.java
@@ -1,8 +1,12 @@
 package com.github.skosmalla.flyway.demo.webapp;
 
-import com.github.skosmalla.flyway.demo.webapp.HomePage;
-import com.github.skosmalla.flyway.demo.webapp.WicketApplication;
+import java.util.Collections;
+import java.util.List;
 
+import com.github.skosmalla.flyway.demo.webapp.domain.Person;
+import com.github.skosmalla.flyway.demo.webapp.repository.PersonRepository;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.apache.wicket.spring.test.ApplicationContextMock;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,23 +14,37 @@ import org.junit.Test;
 /**
  * Simple test using the WicketTester
  */
-public class TestHomePage
-{
-	private WicketTester tester;
+public class TestHomePage {
 
-	@Before
-	public void setUp()
-	{
-		tester = new WicketTester(new WicketApplication());
-	}
+    private WicketTester tester;
 
-	@Test
-	public void homepageRendersSuccessfully()
-	{
-		//start and render the test page
-		tester.startPage(HomePage.class);
+    private ApplicationContextMock applicationContextMock;
 
-		//assert rendered page class
-		tester.assertRenderedPage(HomePage.class);
-	}
+
+    @Before
+    public void setUp() {
+        applicationContextMock = new ApplicationContextMock();
+        WicketApplication wicketApp = new WicketApplication();
+        wicketApp.getComponentInstantiationListeners()
+        .add(new SpringComponentInjector(wicketApp,applicationContextMock));
+        tester = new WicketTester(wicketApp);
+
+        applicationContextMock.putBean(new PersonRepository(){
+            @Override
+            public List<Person> findAllPersons() {
+              return Collections.emptyList();
+            }
+        });
+
+    }
+
+
+    @Test
+    public void homepageRendersSuccessfully() {
+        // start and render the test page
+        tester.startPage(HomePage.class);
+
+        // assert rendered page class
+        tester.assertRenderedPage(HomePage.class);
+    }
 }
