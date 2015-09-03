8c0d38400846c429c8b4490f6bb3aa0651993ec9
sparsick <sparsick@web.de>
2015-09-03 15:57:09 +0200

    clean up; better configuration of flyway cli

--- flyway-demo/database-scripts/src/main/java/db/migration/V1_2_0__Insert_Data_With_Spring_Support.java
+++ flyway-demo/database-scripts/src/main/java/db/migration/V1_2_0__Insert_Data_With_Spring_Support.java
@@ -4,11 +4,11 @@ import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 
-public class V1_2_0__Create_Table_With_Spring_Support implements SpringJdbcMigration {
+public class V1_2_0__Insert_Data_With_Spring_Support implements SpringJdbcMigration {
 
     @Override
     public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
-       jdbcTemplate.execute("Create table address (street Varchar(128), place Varchar(128))");
+       jdbcTemplate.execute("Insert into person (first_name, last_name) Values ('Charlie', 'Delta')");
     }
 
 }
--- flyway-demo/flyway-api-demo/src/main/resources/db/migration/V1_3_0__Insert_Person_Data.sql
+++
@@ -1,1 +0,0 @@
-Insert into person (first_name, last_name) Values ('Bob', 'Marley')
